bfafa4fb1fc7ac10e41566f54e358ba4f9fcaf48
walaa <walaanasser26W@gmail.com>
2022-11-17 12:47:37 +0200

    changed

--- Addict Burger/cartproduct.js
+++ Addict Burger/cartproduct.js
@@ -4,13 +4,20 @@ $('.table tbody').on('click','.btn',function(){
 
 $(document).ready(function(){
   $("#price,#qty").keyup(function(){
- 
-
-var total=0;
- let x=Number($("#price").val());
- let y=Number($("#quantity").val());
-  var total=x*y;
- $("#total").val(total);
+var res=0;
+ let x=parseInt($("#price").val());
+ let y=parseInt($("#quantity").val());
+  var res=x*y;
+ $("#total").val(res);
 
 });
 });
+
+/*
+let n1=document.getElementById("price");
+let n2=document.getElementById("qty");
+let res;
+function myfunction(n1,n2){
+  return paresInt(n1.value)*paresInt(n2.value);
+}
+document.getElementById("total").innerHTML=myfunction(n1,n2);*/
